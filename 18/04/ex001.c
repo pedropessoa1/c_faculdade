cfc739710260cdae462b032d25c7196ddf601343
Pedro Henrique <ph90305@gmail.com>
2022-04-18 12:10:27 -0300

    ex001

--- 18/04/ex001.c
+++ 18/04/ex001.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include<conio.h>
 
 void main (){
+    char c; 
 
+    printf("Digite um caracter: ");
+        c = getche();
+
+    printf("\n Codigo ASCII: %d", c );
+
+
+    printf("\nAntecessor: %c - Codigo ASCCI : %d", c-1,c-1);
+    printf("\nASucessor: %c - Codigo ASCCI : %d", c+1);
 
 }
