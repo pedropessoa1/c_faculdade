40dd8adda94f9ff4b5600e473e9f10f1143efab7
Pedro Henrique <ph90305@gmail.com>
2022-04-25 10:11:21 -0300

    mudei o c

--- 18/04/ex001.c
+++ 18/04/ex001.c
@@ -10,7 +10,7 @@ void main (){
     printf("\n Codigo ASCII: %d", c );
 
 
-    printf("\nAntecessor: %c - Codigo ASCCI : %d", c-1,c-1);
+    printf("\nAnteessor: %c - Codigo ASCCI : %d", c-1,c-1);
     printf("\nASucessor: %c - Codigo ASCCI : %d", c+1);
 
 }
