c38d78af0c07e22bc1b45f141f1b5d377fb01871
Pedro Henrique <ph90305@gmail.com>
2022-05-02 09:53:49 -0300

    atividade

---
+++ atividade_sala/dia_02-05/ex001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+ void main(){
+
+     int vetor[10];
+     int i;
+     for (i = 0; i < 10; i++){
+         printf("\nDigite o valor: ");
+         scanf("%d",vetor[i]);
+     }
+ }
